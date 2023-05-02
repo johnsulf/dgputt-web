e831da6c26c09b1ada515c37e70163b7a3716c59
Erlend Johnsen <erea@ihelse.net>
2023-05-02 12:56:49 +0200

    Footer font size fix

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -8,6 +8,7 @@
             <img src="/images/fb.png" alt="facebook" />
         </a>
     </div>
+    <p class="small-text">Psst... This site is made with Svelte❤️</p>
 </footer>
 
 <style>
@@ -17,4 +18,12 @@
 	.social-media-links img {
 		width: 2rem;
 	}
+    
+    p {
+        font-size: 1.25rem;
+    }
+
+    .small-text {
+        font-size: 0.75rem;
+    }
 </style>
