38e0280cd1c881b0e300ecb33ce2669e5f0a7d01
Erlend Johnsen <erea@ihelse.net>
2023-12-06 10:57:12 +0100

    New components

---
+++ src/components/Home.svelte
@@ -0,0 +1,79 @@
+<script>
+	import Showcase from "./Showcase.svelte";
+	import UserReview from "./UserReview.svelte";
+</script>
+
+<section class="showcase">
+    <Showcase />
+</section>
+<hr />
+<h2>User Reviews</h2>
+<section class="user-reviews">
+    <div class="user-reviews_reviews">
+        <UserReview
+            userName="lolpopli"
+            date="Dec 15, 2022"
+            review="I love this app it really challenges you mentally to get better at putting. You will realize right away how addicting and fun it becomes!!!!"
+            imgSrc="/images/woman_1.png"
+        />
+        <UserReview
+            userName="TheMightyCfresh"
+            date="Sep 3, 2022"
+            review="Just an all around amazing tool for improving your putting. Can't recommend enough."
+            imgSrc="/images/man_1.png"
+        />
+        <UserReview
+            userName="Ken Bare"
+            date="Jan 9, 2022"
+            review="Great App! Tracks my putts and much more. Exactly what I was looking for!"
+            imgSrc="/images/man_2.png"
+        />
+    </div>
+</section>
+
+<style>
+    .showcase {
+        display: grid;
+        gap: 2rem;
+        padding: 1rem 2rem;
+	}
+
+    hr {
+        margin: 1rem 2rem;
+    }
+
+    .user-reviews {
+        padding: 1rem;
+        text-align: center;
+        overflow: scroll;
+    }
+
+    .user-reviews_reviews {
+        display: grid;
+        grid-auto-flow: column;
+        grid-gap: 1rem;
+        margin-right: -150%; /* Hides the extra review to the right */
+        padding-right: 1rem; /* Adds a bit of spacing between the reviews */
+    }
+
+    @media (min-width: 640px) {
+        .showcase {
+            grid-template-columns: 1fr 1fr;
+            padding: 2rem 4rem;
+            flex-wrap: nowrap;
+            align-items: center;
+        }
+
+        .user-reviews {
+            padding: 1rem;
+            text-align: center;
+        }
+
+        .user-reviews_reviews {
+            display: flex;
+            grid-gap: 1rem;
+            margin-right: 0;
+            padding-right: 0;
+        }
+    }
+</style>
---
+++ src/components/PrivacyPolicy.svelte
@@ -0,0 +1,44 @@
+<section class="privacy-policy">
+    <h1>dgputt Privacy Policy</h1>
+    <p>Last updated: 06.12.2023</p>
+
+    <p>This Privacy Policy applies to the dgputt mobile application, provided by FFD700 AS ("we," "us," or "our"). It describes how we collect, use, and share information in connection with your use of the dgputt app (the "Service"). By using the Service, you agree to the collection and use of information in accordance with this policy.</p>
+
+    <h2>Information Collection and Use</h2>
+    <p>For a better experience, while using our Service, we use Google Analytics to collect data regarding app usage and user interactions. This information helps us understand user behavior, improve our Service, and enhance user experience. The data collected by Google Analytics may include your device type, operating system, app usage statistics, performance data, and other anonymized metrics. We do not collect personally identifiable information through Google Analytics.</p>
+
+    <h2>Log Data</h2>
+    <p>In the event of an error in the app, we collect data and information (through third-party products) on your device called Log Data. This may include your device's Internet Protocol (IP) address, device name, operating system version, the configuration of the app when utilizing our Service, the time and date of your use of the Service, and other statistics. This information is used solely for diagnosing and resolving app issues.</p>
+
+    <h2>Cookies</h2>
+    <p>The Service uses third-party code and libraries that use “cookies” to collect information and improve their services. You have the option to accept or refuse these cookies. If you choose to refuse cookies, some portions of this Service may not function as intended.</p>
+
+    <h2>Service Providers</h2>
+    <p>We may employ third-party companies and individuals to facilitate our Service or perform Service-related services. These third parties have access to non-personal data solely for performing these tasks on our behalf and are obligated not to disclose or use the information for any other purpose.</p>
+
+    <h2>Security</h2>
+    <p>We are committed to protecting your information. We use commercially acceptable means of protecting non-personal data but remember that no method of electronic storage or transmission over the internet is 100% secure.</p>
+
+    <h2>Links to Other Sites</h2>
+    <p>Our Service may contain links to other sites. We have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.</p>
+
+    <h2>Children's Privacy</h2>
+    <p>Our Service does not address anyone under the age of 13. We do not knowingly collect personal information from children under 13. If we become aware that a child under 13 has provided us with personal information, we take steps to remove such information and terminate the child's account.</p>
+
+    <h2>Changes to This Privacy Policy</h2>
+    <p>We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page. You are advised to review this Privacy Policy periodically for any changes.</p>
+
+    <h2>Contact Us</h2>
+    <p>If you have any questions or suggestions about our Privacy Policy, please contact us at <a href="mailto:contact@dgputt.app">contact@dgputt.app</a>.</p>
+
+    <p>This Privacy Policy was created for FFD700 AS.</p>
+</section>
+
+<style>
+    .privacy-policy {
+        max-width: 800px;
+        margin: auto;
+        padding: 1rem;
+        text-align: left;
+    }
+</style>
