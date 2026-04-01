9e5839d6296a43c8fa3e9339ced2761e59b9d271
johnsulf <erlendjohns@gmail.com>
2026-04-01 08:34:51 +0200

    deletes svelte app and moves next.js app to root

--- src/routes/routes.js
+++
@@ -1,9 +0,0 @@
-import Home from "../components/Home.svelte";
-import PrivacyPolicy from "../components/PrivacyPolicy.svelte";
-
-const routes = {
-    "/": Home,
-    "/privacy-policy": PrivacyPolicy,
-  };
-
-  export default routes;
