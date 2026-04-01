a4e3d66c68ae9d6e54df67383534c9dc96ef6e6c
johnsulf <erlendjohns@gmail.com>
2026-04-01 09:45:59 +0200

    feat: enhance privacy policy page structure and styling

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,104 +1,122 @@
 export default function PrivacyPolicy() {
   return (
-    <section className="max-w-200 mx-auto p-4 text-left">
-      <h1 className="text-4xl font-bold">dgputt Privacy Policy</h1>
-      <p className="my-4">Last updated: 06.12.2023</p>
-
-      <p className="my-4">
+    <article className="max-w-200 mx-auto p-4 text-left space-y-8">
+      <h1>dgputt Privacy Policy</h1>
+      <p>
+        Last updated: <time dateTime="2023-12-06">06.12.2023</time>
+      </p>
+      <p>
         This Privacy Policy applies to the dgputt mobile application, provided
-        by FFD700 AS (&quot;we,&quot; &quot;us,&quot; or &quot;our&quot;). It
-        describes how we collect, use, and share information in connection with
-        your use of the dgputt app (the &quot;Service&quot;). By using the
-        Service, you agree to the collection and use of information in
-        accordance with this policy.
+        by FFD700 AS ("we," "us," or "our"). It describes how we collect, use,
+        and share information in connection with your use of the dgputt app (the
+        "Service"). By using the Service, you agree to the collection and use of
+        information in accordance with this policy.
       </p>
 
-      <h2 className="text-2xl font-bold mt-8">
-        Information Collection and Use
-      </h2>
-      <p className="my-4">
-        For a better experience, while using our Service, we use Google
-        Analytics to collect data regarding app usage and user interactions.
-        This information helps us understand user behavior, improve our Service,
-        and enhance user experience. The data collected by Google Analytics may
-        include your device type, operating system, app usage statistics,
-        performance data, and other anonymized metrics. We do not collect
-        personally identifiable information through Google Analytics.
-      </p>
+      <article>
+        <h2>Information Collection and Use</h2>
+        <p>
+          For a better experience, while using our Service, we use Google
+          Analytics to collect data regarding app usage and user interactions.
+          This information helps us understand user behavior, improve our
+          Service, and enhance user experience. The data collected by Google
+          Analytics may include your device type, operating system, app usage
+          statistics, performance data, and other anonymized metrics. We do not
+          collect personally identifiable information through Google Analytics.
+        </p>
+      </article>
 
-      <h2 className="text-2xl font-bold mt-8">Log Data</h2>
-      <p className="my-4">
-        In the event of an error in the app, we collect data and information
-        (through third-party products) on your device called Log Data. This may
-        include your device&apos;s Internet Protocol (IP) address, device name,
-        operating system version, the configuration of the app when utilizing
-        our Service, the time and date of your use of the Service, and other
-        statistics. This information is used solely for diagnosing and resolving
-        app issues.
-      </p>
+      <article>
+        <h2>Log Data</h2>
+        <p>
+          In the event of an error in the app, we collect data and information
+          (through third-party products) on your device called Log Data. This
+          may include your device&apos;s Internet Protocol (IP) address, device
+          name, operating system version, the configuration of the app when
+          utilizing our Service, the time and date of your use of the Service,
+          and other statistics. This information is used solely for diagnosing
+          and resolving app issues.
+        </p>
+      </article>
 
-      <h2 className="text-2xl font-bold mt-8">Cookies</h2>
-      <p className="my-4">
-        The Service uses third-party code and libraries that use
-        &quot;cookies&quot; to collect information and improve their services.
-        You have the option to accept or refuse these cookies. If you choose to
-        refuse cookies, some portions of this Service may not function as
-        intended.
-      </p>
+      <article>
+        <h2>Cookies</h2>
+        <p>
+          The Service uses third-party code and libraries that use "cookies" to
+          collect information and improve their services. You have the option to
+          accept or refuse these cookies. If you choose to refuse cookies, some
+          portions of this Service may not function as intended.
+        </p>
+      </article>
 
-      <h2 className="text-2xl font-bold mt-8">Service Providers</h2>
-      <p className="my-4">
-        We may employ third-party companies and individuals to facilitate our
-        Service or perform Service-related services. These third parties have
-        access to non-personal data solely for performing these tasks on our
-        behalf and are obligated not to disclose or use the information for any
-        other purpose.
-      </p>
+      <article>
+        <h2>Service Providers</h2>
+        <p>
+          We may employ third-party companies and individuals to facilitate our
+          Service or perform Service-related services. These third parties have
+          access to non-personal data solely for performing these tasks on our
+          behalf and are obligated not to disclose or use the information for
+          any other purpose.
+        </p>
+      </article>
 
-      <h2 className="text-2xl font-bold mt-8">Security</h2>
-      <p className="my-4">
-        We are committed to protecting your information. We use commercially
-        acceptable means of protecting non-personal data but remember that no
-        method of electronic storage or transmission over the internet is 100%
-        secure.
-      </p>
+      <article>
+        <h2>Security</h2>
+        <p>
+          We are committed to protecting your information. We use commercially
+          acceptable means of protecting non-personal data but remember that no
+          method of electronic storage or transmission over the internet is 100%
+          secure.
+        </p>
+      </article>
 
-      <h2 className="text-2xl font-bold mt-8">Links to Other Sites</h2>
-      <p className="my-4">
-        Our Service may contain links to other sites. We have no control over
-        and assume no responsibility for the content, privacy policies, or
-        practices of any third-party sites or services.
-      </p>
+      <article>
+        <h2>Links to Other Sites</h2>
+        <p>
+          Our Service may contain links to other sites. We have no control over
+          and assume no responsibility for the content, privacy policies, or
+          practices of any third-party sites or services.
+        </p>
+      </article>
 
-      <h2 className="text-2xl font-bold mt-8">Children&apos;s Privacy</h2>
-      <p className="my-4">
-        Our Service does not address anyone under the age of 13. We do not
-        knowingly collect personal information from children under 13. If we
-        become aware that a child under 13 has provided us with personal
-        information, we take steps to remove such information and terminate the
-        child&apos;s account.
-      </p>
+      <article>
+        <h2>Children&apos;s Privacy</h2>
+        <p>
+          Our Service does not address anyone under the age of 13. We do not
+          knowingly collect personal information from children under 13. If we
+          become aware that a child under 13 has provided us with personal
+          information, we take steps to remove such information and terminate
+          the child&apos;s account.
+        </p>
+      </article>
 
-      <h2 className="text-2xl font-bold mt-8">
-        Changes to This Privacy Policy
-      </h2>
-      <p className="my-4">
-        We may update our Privacy Policy from time to time. We will notify you
-        of any changes by posting the new Privacy Policy on this page. You are
-        advised to review this Privacy Policy periodically for any changes.
-      </p>
+      <article>
+        <h2>Changes to This Privacy Policy</h2>
+        <p>
+          We may update our Privacy Policy from time to time. We will notify you
+          of any changes by posting the new Privacy Policy on this page. You are
+          advised to review this Privacy Policy periodically for any changes.
+        </p>
+      </article>
 
-      <h2 className="text-2xl font-bold mt-8">Contact Us</h2>
-      <p className="my-4">
-        If you have any questions or suggestions about our Privacy Policy,
-        please contact us at{" "}
-        <a href="mailto:contact@dgputt.app" className="underline text-blue-700">
-          contact@dgputt.app
-        </a>
-        .
-      </p>
+      <hr />
 
-      <p className="my-4">This Privacy Policy was created for FFD700 AS.</p>
-    </section>
+      <article>
+        <h2>Contact Us</h2>
+        <p>
+          If you have any questions or suggestions about our Privacy Policy,
+          please contact us at{" "}
+          <a
+            href="mailto:contact@dgputt.app"
+            className="underline text-blue-700"
+          >
+            contact@dgputt.app
+          </a>
+          .
+        </p>
+      </article>
+
+      <p>This Privacy Policy was created for FFD700 AS.</p>
+    </article>
   );
 }
