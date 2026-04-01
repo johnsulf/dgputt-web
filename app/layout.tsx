2885cb23e7cd98441cbdccfcbaaee536a9c298ce
johnsulf <erlendjohns@gmail.com>
2026-04-01 08:56:43 +0200

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Inter, Space_Grotesk } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const spaceGroteskHeading = Space_Grotesk({subsets:['latin'],variable:'--font-heading'});
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "dgputt - The disc golf putting app",
@@ -13,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", inter.variable, spaceGroteskHeading.variable)}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
